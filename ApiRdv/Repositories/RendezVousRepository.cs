3744db9904872ae640ec367445476a94077ba0f8
Philippe CHARLES <A114SR@login.axa>
2024-02-01 18:31:31 +0100

    implementation du code dans RendezVousRepository, dans RendezvousDbContext et dans Models/ RendezVouscs

--- ApiRdv/Repositories/RendezVousRepository.cs
+++ ApiRdv/Repositories/RendezVousRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace ApiRdv.Repositories
+﻿using ApiRdv.Data;
+using ApiRdv.Models;
+
+namespace ApiRdv.Repositories
 {
     public class RendezVousRepository
     {
+        private readonly RendezVousDbContext _dbcontext;
+
+        public RendezVousRepository(RendezVousDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        public IEnumerable<RendezVous> GetAll()
+        {
+            return _dbcontext.RendezVous.ToList();
+        }
+        public RendezVous GetById(int id)
+        {
+            return _dbcontext.RendezVous.Find(id);
+        }
+
+        public void Add(RendezVous rendezVous)
+        {
+            _dbcontext.RendezVous.Add(rendezVous);
+            _dbcontext.SaveChanges();
+        }
+
+        public void Update(RendezVous rendezVous)
+        {
+            _dbcontext.RendezVous.Update(rendezVous);
+            _dbcontext.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var rendezVous = _dbcontext.RendezVous.Find(id);
+            if (rendezVous != null)
+            {
+                _dbcontext.RendezVous.Remove(rendezVous);
+                _dbcontext.SaveChanges();
+            }
+        }
     }
 }
