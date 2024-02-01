4bdb938246b4c9d2e77e15fad917289bc5353542
Philippe CHARLES <A114SR@login.axa>
2024-02-01 19:51:05 +0100

    implementation du code dans RendezVousService.cs

--- ApiRdv/Services/RendezVousService.cs
+++ ApiRdv/Services/RendezVousService.cs
@@ -1,6 +1,46 @@
-﻿namespace ApiRdv.Services
+﻿using ApiRdv.Models;
+using ApiRdv.Repositories;
+
+namespace ApiRdv.Services
 {
     public class RendezVousService
     {
+
+        private readonly RendezVousRepository _repository;
+
+public RendezVousService(RendezVousRepository repository)
+        {
+            _repository = repository;
+        }
+        public IEnumerable<RendezVous> GetAllRendezvous()
+        {
+            return _repository.GetAll();
+        }
+        public RendezVous GetRendezVousById(int id) 
+        {
+         return _repository.GetById(id);
+        }
+        public void CreateRendezVous(RendezVous rendezVous) 
+        {
+            // Ajouter des logiques de validation si nécessaire
+            rendezVous.Date = DateTime.Now;// Exemple : assigner la date actuelle lors de la création
+            _repository.Add(rendezVous);
+        }
+        public void UpdateRendezVous(int id, RendezVous updateDRendezVous)
+        {
+            var existingRendezVous= _repository.GetById(id);
+
+            if (existingRendezVous != null)
+            {
+                existingRendezVous.NomPatient = updateDRendezVous.NomPatient;
+                existingRendezVous.Date = updateDRendezVous.Date;
+
+                _repository.Update(existingRendezVous);
+            }
+        }
+
+        public void DeleteRendezVous(int Id)
+        { _repository.Delete(Id); }
+
     }
 }
