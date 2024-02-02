886f99f36bd5ec34993f09027d3bf54dcbb20034
Philippe CHARLES <A114SR@login.axa>
2024-02-02 10:45:06 +0100

    implémentation de l'apigateway

---
+++ ApiGateway/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Ocelot.DependencyInjection;
+
+namespace ApiGateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+                //Configuration Swagger
+                services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "MicroserviceApiGateway", Version = "v1" });
+                });
+            // Configuration Ocelot
+            services.AddOcelot(Configuration.GetSection("Ocelot"));
+        }
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();   
+            }
+            app.UseRoutin
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MicroserviceApiGateway V1");
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
